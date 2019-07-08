e4b6f172e6257a2b29cb92688190662edf7d7ee3
Michael Gaylord <mjgaylord@gmail.com>
2019-07-08 10:51:41 +0800

    Better error handling

--- handler.ts
+++ handler.ts
@@ -10,7 +10,7 @@ interface Response {
   isBase64Encoded: boolean
 }
 
-export const hello: APIGatewayProxyHandler = async (_event: any = {}, _context: Context, callback: Callback): Promise<any> => {
+export const hello: APIGatewayProxyHandler = async (_event: any = {}, _context: Context, _callback: Callback): Promise<any> => {
   dotenv.config()
   // we subtract 2 days as the previous day may not yet be available resulting in an empty response
   const yesterday = moment().subtract(2, 'days').format('YYYY-MM-DD')
@@ -20,29 +20,35 @@ export const hello: APIGatewayProxyHandler = async (_event: any = {}, _context: 
   }
   const api = axios.create({
     baseURL: "https://api2.branch.io/v3"
-  });
-  const response = await api.post('/export/', {
-    branch_key: process.env.BRANCH_KEY,
-    branch_secret: process.env.BRANCH_SECRET,
-    export_date: yesterday
   })
-  if (response instanceof Error) {
-    callback(JSON.stringify(response), null)
-    throw response
+  try {
+    const response = await api.post('/export/', {
+      branch_key: process.env.BRANCH_KEY,
+      branch_secret: process.env.BRANCH_SECRET,
+      export_date: yesterday
+    })
+    const result: Response = {
+      statusCode: 200,
+      body: JSON.stringify(translateResponse(response.data)),
+      isBase64Encoded: false
+    }
+    console.debug('Exports requested successfully...')
+    return result
+  } catch (error) {
+    const failed: Response = {
+      statusCode: 400,
+      body: (error.message || 'Unknown error'),
+      isBase64Encoded: false
+    }
+    return failed
   }
-  const result: Response = {
-    statusCode: 200,
-    body: JSON.stringify(translateResponse(response.data)),
-    isBase64Encoded: false
-  }
-  console.debug('Exports requested successfully...')
-  return result
 }
 
 function translateResponse(response: JSON): string[] {
   let result = Array<string>()
   for(const key in response) {
-    result.push(response[key])
+    result.push(...response[key])
   }
+  console.debug(`${JSON.stringify(result)}`)
   return result
 }

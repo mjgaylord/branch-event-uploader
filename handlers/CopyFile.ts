8cfbfa53f79485c427511603d7ab2f132a9a307c
Michael Gaylord <mjgaylord@gmail.com>
2019-08-09 12:44:06 +0800

    Add gunzip support

--- handlers/CopyFile.ts
+++ handlers/CopyFile.ts
@@ -1,9 +1,12 @@
 import { APIGatewayProxyHandler, Context, Callback, APIGatewayEvent } from 'aws-lambda'
 import 'source-map-support/register'
-import fetch from 'node-fetch'
 import * as pathUtil from 'path'
 import { s3 } from '../config/Config'
-import { Database } from './../database/Database'
+import { Database } from '../database/Database'
+import * as zlib from 'zlib'
+import * as https from 'https'
+import { Response } from '../model/Models'
+
 
 const database = new Database()
 
@@ -22,53 +25,63 @@ export const run: APIGatewayProxyHandler = async (event: APIGatewayEvent, contex
   const destBucket = bucket
   context.callbackWaitsForEmptyEventLoop = true
 
-  function downloadAsync() {
-    return new Promise<any>(async (resolve, reject) => {
-      const response = await fetch(path)
-      console.log(`Downloading file: ${path}`)
-      if (!response.ok) {
-        const result = {
-          statusCode: 404,
-          body: `Unable to locate: ${path}`
-        }
-        reject(result)
-        return
-      }
+  console.info(`File: ${key} written to bucket: ${destBucket}`)
+  try {
+    const data = await getUncompressed(path)
+    await uploadToS3(data, destBucket, key, path)
+    await database.downloadCompleted(path)
+    const result: Response = {
+      statusCode: 200,
+      body: path,
+      isBase64Encoded: false
+    }
+    return result
+  } catch (error) {
+    console.error(`Error downloading and unzipping file on S3: ${JSON.stringify(error)}`)
+    const failed: Response = {
+      statusCode: 400,
+      body: (error.message || 'Unknown error'),
+      isBase64Encoded: false
+    }
+    return failed
+  } finally {
+    context.done(null, "success");
+  }
+}
 
-      console.log('Downloaded file successfully, writing to S3...')
-      const buffer = await response.buffer()
-      s3.upload({
-        Bucket: destBucket,
-        Key: key,
-        Body: buffer,
-        Metadata: { downloadPath: path }
-      }, async function (err, _data) {
-        if (err) {
-          console.error(`Error uploading: ${err}`)
-          reject(err)
-          throw err
-        }
-        console.info(`File: ${key} written to bucket: ${destBucket}`)
-
-        //update dynamoDb
-        try {
-          await database.downloadCompleted(path)
-          resolve("success")
-        } catch (error) {
-          reject(error)
-          return
-        }
+function getUncompressed(path: string): Promise<string> {
+  return new Promise<any>(async (resolve, reject) => {
+    https.get(path, response => {
+      const unzip = zlib.createGunzip()
+      const buffer = []
+      unzip.on('data', data => {
+        buffer.push(data.toString())
+      }).on('end', () => {
+        const uncompressed = buffer.join("")
+        console.log('Downloaded file successfully, writing to S3...')
+        resolve(uncompressed)
       })
+      response.pipe(unzip)
+    }).on('error', error => {
+      reject(error)
     })
-  }
+  })
+}
 
-  await downloadAsync()
-  context.done(null, "success");
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      input: event.queryStringParameters,
+function uploadToS3(csv: string, bucket: string, key: string, path: string): Promise<boolean> {
+  return new Promise<any>((resolve, reject) => {
+    s3.upload({
+      Bucket: bucket,
+      Key: key,
+      Body: csv,
+      Metadata: { downloadPath: path }
+    }, async function (err, _data) {
+      if (!!err) {
+        console.error(`Error uploading: ${err}`)
+        reject(err)
+        throw err
+      }
+      resolve(true)
     })
-  }
+  })
 }

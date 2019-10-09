f0aaaf888927691e2bfd74f5c5c5eacd430a6d69
Michael Gaylord <mjgaylord@gmail.com>
2019-10-09 09:35:06 +0800

    Fix logic

--- src/event-uploaders/UploaderFunctions.ts
+++ src/event-uploaders/UploaderFunctions.ts
@@ -1,6 +1,6 @@
 import { excludedTopics } from "../utils/Config"
 
 export function shouldUpload(filename: string, config: string): Boolean {
-  const topics = excludedTopics(config).filter(topic => filename.indexOf(topic))
+  const topics = excludedTopics(config).filter(topic => filename.indexOf(topic) >= 0)
   return topics.length === 0
 }

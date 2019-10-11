830ec02e64aa810685964fd4df11d99b3b1432f7
Michael Gaylord <mjgaylord@gmail.com>
2019-10-11 10:16:09 +0800

    Minor fixes

--- src/utils/StringUtils.ts
+++ src/utils/StringUtils.ts
@@ -1,7 +1,7 @@
 
 export function dateInFilename(filename: string): string {
   const matches = filename.match('[0-9]{4}[-|\/]{1}[0-9]{2}[-|\/]{1}[0-9]{2}')
-  if (matches.length === 0) {
+  if (!matches || matches.length === 0) {
     return "Unknown"
   }
   return matches[0]
